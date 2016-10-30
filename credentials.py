a7a59757a422bacdedd40af9e4167011f697f832
Joe Weiner <undertaker0420@gmail.com>
2016-10-30 14:33:33 -0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -4,7 +4,7 @@
 # 2. Make sure your mobile number is listed at twitter.com/settings/devices
 # 3. Head to apps.twitter.com and select Keys and Access Tokens
 
-CONSUMER_KEY = 'iLwhpUWO2K5I1CeNPpKIusa61'
+CONSUMER_KEY = 'cs7oDprWACHtinTZm7RvfPbYe'
 #Secret Token Taken Out of public repo
 # Create a new Access Token
 ACCESS_TOKEN = '792172843324899329-8OE695NCOfJ7toMqa2BEBTRvFcRXAOt'
